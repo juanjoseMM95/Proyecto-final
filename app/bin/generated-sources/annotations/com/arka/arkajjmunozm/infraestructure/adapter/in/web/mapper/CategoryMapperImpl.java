90224870225c560fad251d2cf8fbbd039f48f733
juanjoseMM95 <94885709+juanjoseMM95@users.noreply.github.com>
2025-09-30 22:14:03 -0500

    add: unversioned files

---
+++ app/bin/generated-sources/annotations/com/arka/arkajjmunozm/infraestructure/adapter/in/web/mapper/CategoryMapperImpl.java
@@ -0,0 +1,11 @@
+package com.arka.arkajjmunozm.infraestructure.adapter.in.web.mapper;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-30T22:05:17-0500",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.43.0.v20250819-1513, environment: Java 21.0.8 (Eclipse Adoptium)"
+)
+public class CategoryMapperImpl implements CategoryMapper {
+}
